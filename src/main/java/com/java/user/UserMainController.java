63f77be2f9ebec9ae49d361a75b350a19eb765cf
babu9985 <61941088+babu9985@users.noreply.github.com>
2021-07-17 11:56:24 +0530

    Add files via upload
    uploading project

---
+++ src/main/java/com/java/user/UserMainController.java
@@ -0,0 +1,15 @@
+package com.java.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMainController {
+
+	public static void main(String[] args)  
+	{    
+	SpringApplication.run(UserMainController.class, args);    
+	}   
+	}  
+
+
